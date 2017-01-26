19dc2e85ce714c9d080004c3f1ebb897074329d6
Scott L. Price <prices@hugllc.com>
2017-01-25 20:26:36 -0600

    Start of the SSL code.  I added all the functions to try to have the SSL code work right next to the other code.

--- core/httpd-nonos.c
+++ core/httpd-nonos.c
@@ -11,7 +11,9 @@ ESP8266 web server - platform-dependent routines, nonos version
 
 //Listening connection data
 static struct espconn httpdConn;
+static struct espconn httpdSSLConn;
 static esp_tcp httpdTcp;
+static esp_tcp httpdSSLTcp;
 
 //Set/clear global httpd lock.
 //Not needed on nonoos.
@@ -53,31 +55,63 @@ static void ICACHE_FLASH_ATTR platConnCb(void *arg) {
 		espconn_regist_disconcb(conn, platDisconCb);
 		espconn_regist_sentcb(conn, platSentCb);
 	} else {
-#ifdef USE_SSL
-        espconn_secure_disconnect(conn);
-#else        
 		espconn_disconnect(conn);
-#endif
 	}
+}
+
+static void ICACHE_FLASH_ATTR platReconSSLCb(void *arg, sint8 err) {
+    //From ESP8266 SDK
+    //If still no response, considers it as TCP connection broke, goes into espconn_reconnect_callback.
+    
+    ConnTypePtr conn=arg;
+    //Just call disconnect to clean up pool and close connection.
+    httpdDisconSSLCb(conn, (char*)conn->proto.tcp->remote_ip, conn->proto.tcp->remote_port);
+}
+
+static void ICACHE_FLASH_ATTR platDisconSSLCb(void *arg) {
+    ConnTypePtr conn=arg;
+    httpdDisconSSLCb(conn, (char*)conn->proto.tcp->remote_ip, conn->proto.tcp->remote_port);
+}
+
+static void ICACHE_FLASH_ATTR platRecvSSLCb(void *arg, char *data, unsigned short len) {
+    ConnTypePtr conn=arg;
+    httpdRecvSSLCb(conn, (char*)conn->proto.tcp->remote_ip, conn->proto.tcp->remote_port, data, len);
+}
+
+static void ICACHE_FLASH_ATTR platSentSSLCb(void *arg) {
+    ConnTypePtr conn=arg;
+    httpdSentSSLCb(conn, (char*)conn->proto.tcp->remote_ip, conn->proto.tcp->remote_port);
+}
+
+static void ICACHE_FLASH_ATTR platConnSSLCb(void *arg) {
+    ConnTypePtr conn=arg;
+    if (httpdConnectSSLCb(conn, (char*)conn->proto.tcp->remote_ip, conn->proto.tcp->remote_port)) {
+        espconn_regist_recvcb(conn, platRecvCb);
+        espconn_regist_reconcb(conn, platReconCb);
+        espconn_regist_disconcb(conn, platDisconCb);
+        espconn_regist_sentcb(conn, platSentCb);
+    } else {
+        espconn_secure_disconnect(conn);
+    }
 }
 
 
 int ICACHE_FLASH_ATTR httpdPlatSendData(ConnTypePtr conn, char *buff, int len) {
 	int r;
-#ifdef USE_SSL
-    r=espconn_secure_send(conn, (uint8_t*)buff, len);
-#else
-    r=espconn_sent(conn, (uint8_t*)buff, len);
-#endif
+    if (conn == &httpdSSLConn) {
+        r=espconn_secure_send(conn, (uint8_t*)buff, len);
+    } else {
+        r=espconn_sent(conn, (uint8_t*)buff, len);
+    }
     return (r>=0);
 }
 
 void ICACHE_FLASH_ATTR httpdPlatDisconnect(ConnTypePtr conn) {
-#ifdef USE_SSL
-    espconn_secure_disconnect(conn);
-#else        
-    espconn_disconnect(conn);
-#endif
+    if (conn == &httpdSSLConn) {
+        espconn_secure_disconnect(conn);
+    } else {
+        espconn_disconnect(conn);
+    }
 }
 
 void ICACHE_FLASH_ATTR httpdPlatDisableTimeout(ConnTypePtr conn) {
@@ -92,14 +126,21 @@ void ICACHE_FLASH_ATTR httpdPlatInit(int port, int maxConnCt) {
 	httpdTcp.local_port=port;
 	httpdConn.proto.tcp=&httpdTcp;
 	espconn_regist_connectcb(&httpdConn, platConnCb);
-#ifdef USE_SSL
+    espconn_accept(&httpdConn);
+    espconn_tcp_set_max_con_allow(&httpdConn, maxConnCt);
+    
+}
+//Initialize listening socket, do general initialization
+void ICACHE_FLASH_ATTR httpdPlatSSLInit(int port, int maxConnCt) {
+    httpdSSLConn.type=ESPCONN_TCP;
+    httpdSSLConn.state=ESPCONN_NONE;
+    httpdSSLTcp.local_port=port;
+    httpdSSLConn.proto.tcp=&httpdSSLTcp;
+    espconn_regist_connectcb(&httpdSSLConn, platConnCb);
     //espconn_secure_set_default_certificate(default_certificate, default_certificate_len);
     //espconn_secure_set_default_private_key(default_private_key, default_private_key_len);
-    espconn_secure_accept(&httpdConn);
-#else
-    espconn_accept(&httpdConn);
-#endif
-    espconn_tcp_set_max_con_allow(&httpdConn, maxConnCt);
+    espconn_secure_accept(&httpdSSLConn);
+    espconn_tcp_set_max_con_allow(&httpdSSLConn, maxConnCt);
     
 }
 
